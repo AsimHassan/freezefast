41086e2f834751da7a9a3623d5b4972fef295bfb
Asim <asimhassan939@gmail.com>
2021-08-10 12:40:04 +0530

    ESP32-ROVER_Code

---
+++ freezefast-client-rover/include/configs.h
@@ -0,0 +1,29 @@
+
+
+#ifndef CONFIGS_H
+#define CONFIGS_H
+/*****************WiFi Credentials**********************/
+
+#define WIFI_SSID "BSNLFTTH-880"
+#define WIFI_PSD "4872962880"
+/*****************Server details**********************/
+
+#define SERVER "192.168.3.64"
+#define PORT 1234
+/*****************Pins**********************/
+
+#define FORWARD_PIN 19
+#define REVERSE_PIN 18
+#define SLOWDOWN_PIN 5
+#define BUZZER_PIN 4
+#define OBSTACLE_SENSOR_F_PIN 33
+#define OBSTACLE_SENSOR_B_PIN 25
+#define LIMIT_SWITCH_F_PIN 27
+#define LIMIT_SWITCH_B_PIN 26
+/**********************STATION PARAMETERS***********/
+#define ROVER_ID 1
+
+/********************other parameters**************/
+
+#define LED_BLINK_DELAY 1000
+#endif
