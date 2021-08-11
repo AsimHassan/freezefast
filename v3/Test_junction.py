19240aaadea4b74f143dd72f77d54eb008f66c8f
Asim <asimhassan939@gmail.com>
2021-08-11 22:12:50 +0530

    Junction code+ test

--- v3/Test_junction.py
+++ v3/Test_junction.py
@@ -1,6 +1,18 @@
 import asyncio
 import aioconsole
-import rover
+states = {
+    'STATE:0':"IDLE",
+    'STATE:1':"ROVER_CROSS",
+    'STATE:2':"ROVER_CROSS_INTER",
+    'STATE:3':"ROVER_CROSS2",
+    'STATE:4':"ROVER_HERE",
+    'STATE:5':"ROTATE",
+    'STATE:6':"ROVER_READY_TO_LEAVE",
+    'STATE:7':"ROVER_LEAVING",
+    'STATE:8':"ROVER_LEAVING2",
+    'STATE:9':"ROVER_LEFT",
+    'STATE:10':"JUNCTION_ERROR"
+}
 ip = '192.168.3.64'
 portno = 1234
 async def Test_rover(reader:asyncio.StreamReader,writer:asyncio.StreamWriter):
@@ -14,28 +26,38 @@ async def Test_rover(reader:asyncio.StreamReader,writer:asyncio.StreamWriter):
           """)
     try:
         while True:
+            await asyncio.sleep(0.1)
             try:
-                choice = await asyncio.wait_for(aioconsole.ainput(),0.5)
+                choice = await asyncio.wait_for(aioconsole.ainput(""),0.5)
             except asyncio.TimeoutError:
                 choice = '0'
             except:
                 raise
 
             if choice == '1':
-                await writer.write("ROTATE|CROSS")
+                writer.write("ROTATE|CROSS.".encode())
             if choice == '2':
-                await writer.write("ROTATE|STRAIGHT")
+                writer.write("ROTATE|STRAIGHT.".encode())
+
             try :
-                message = await asyncio.wait_for(rover_obj.update_state(),0.5)
-                if message:
+                message = await asyncio.wait_for(reader.readuntil(".".encode()),0.2)
+                if len(message) > 0:
                     message = message.decode()
-                    print(message)
+                    message.strip()
+                    message = message[:-1]
+
+                    await aioconsole.aprint(message,end=":")
+
+                    message = message.split("|")
+                    if 'STATE' in message[-1]:
+                        await aioconsole.aprint(states[message[-1]])
             except asyncio.TimeoutError:
                 pass
             except:
                 raise
     except :
         print("lost connection")
+        raise
 
 async def main():
     server = await asyncio.start_server(Test_rover,ip,portno)
