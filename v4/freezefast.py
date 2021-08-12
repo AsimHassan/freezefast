f592f14f10a509428b79a669d411675bc2799d6f
Asim <asimhassan939@gmail.com>
2021-08-12 15:59:01 +0530

    added rover and junction to logic

--- v4/freezefast.py
+++ v4/freezefast.py
@@ -19,7 +19,7 @@ class Msgpriority:
     GO = 5
     ACK = 8
 
-class RoverStates():
+class RoverStates(Enum):
     RESET = 0
     RESTING = 1
     SLOWDOWN = 2
@@ -30,8 +30,9 @@ class RoverStates():
     EMERGENCY_STOP = 7
     REVERSE = 9
     FORWARD = 8
+    BLOCKED_TILL_UPDATE = 99
 
-class JunctionStates():
+class JunctionStates(Enum):
     IDLE = 0
     ROVER_CROSS = 1
     ROVER_CROSS_INTER = 2
@@ -56,6 +57,13 @@ class StationStates():
     FORWARD = 8
     REVERSE = 9
 
+def get_direction(position, destination):
+    if destination - position > 0:
+        return "FORWARD"
+    elif destination - position < 0:
+        return "REVERSE"
+    else:
+        return "STOP"
 
 
 
@@ -71,8 +79,23 @@ class Freezefast:
         self.callq = queue.Queue()
         self.rover_position = positions["1"]
         self.rover_destination = positions["1"]
-        self.rover_state = RoverStates.RESET
-        self.junction_state = JunctionStates.IDLE
+        self.rover_state = RoverStates.RESET.value
+        self.junction_state = JunctionStates.IDLE.value
+        self.junction_position = 'STRAIGHT'
+
+    def handle_call_queue(self):
+        if self.rover_state == RoverStates.RESTING.value:
+            if not self.callq.empty():
+                destination = self.callq.get()
+                if int(destination) != positions["JUNCTION"]:
+                    direction = get_direction(self.rover_position,destination)
+                    return [(Msgpriority.GO,self.ROVER,direction)]
+                else:
+                    #TODO Junction logicj
+                    pass
+                
+        return []
+
 
     def parse_message(self,msg:str):
         splitmsg = msg.split('|')
@@ -88,76 +111,138 @@ class Freezefast:
             (destination,message) = self.parse_hmi(splitmsg[2:]) 
 
     def parse_station(self,msg:list):
+        sendbackaddr = "|".join(msg[:2])
         #TODO Parse CALL
         if 'CALL' in msg:
-            return self._parse_station_call(msg) # return list of tuple
+            return self._parse_station_call(msg,sendbackaddr) # return list of tuple
 
 
         #TODO Parse GO
         if 'GO' in msg:
-            return self._parse_station_go(msg) # return list of tuple
+            return self._parse_station_go(msg,sendbackaddr) # return list of tuple
 
 
         #TODO Parse ROVERCROSS
+        if 'ROVERCROSS' in msg:
+            return self._parse_station_rovercross(msg,sendbackaddr)
         #TODO Parse SLOWDOWN
         if 'SLOWDOWN' in msg:
-            return self._parse_station_slowdown(msg)
+            return self._parse_station_slowdown(msg,sendbackaddr)
         #TODO Parse STOP
         if 'STOP' in msg:
-            return self._parse_station_stop(msg) 
+            return self._parse_station_stop(msg,sendbackaddr) 
         #TODO Emergency STOP
         #TODO RESTART
 
 
-    def _parse_station_call(self, msg):
+    def _parse_station_call(self, msg,sendbackaddr):
         called_station = msg[1]
         self.callq.put(positions[called_station])
-        msg_destination_station = "|".join(msg[:2])
         msg_call_ack = 'CALL|ACK'
         msg_priority = Msgpriority.ACK
-        return [(msg_priority,msg_destination_station,msg_call_ack)]
+        return [(msg_priority,sendbackaddr,msg_call_ack)]
     
-    def _parse_station_go(self, msg):
-        msg_destination_station = "|".join(msg[:2])
+    def _parse_station_go(self, msg,sendbackaddr):
         msg_go_ack = 'GO|ACK'
-        msg_priority = Msgpriority.ACK
-        return [(4,self.ROVER,'FORWARD'),(msg_priority,msg_destination_station,msg_go_ack)]
+        self.rover_destination = positions["COLDROOM"]
+        direction = get_direction(self.rover_position,self.rover_destination)
+        to_rover = (Msgpriority.GO,self.ROVER,direction)
+        to_station = (Msgpriority.ACK,sendbackaddr,msg_go_ack)
+        return [to_rover,to_station]
     
-    def _parse_station_slowdown(self,msg):
-        to_station = (Msgpriority.ACK,"|".join(msg[:2],'SLOWDOWN|ACK'))
+    def _parse_station_slowdown(self,msg,sendbackaddr):
+        to_station = (Msgpriority.ACK,sendbackaddr,'SLOWDOWN|ACK')
         to_rover = (Msgpriority.SLOWDOWN,self.ROVER,'SLOWDOWN')
         return[to_rover,to_station]
          
-    def _parse_station_stop(self,msg):
-        msg_destination_station = "|".join(msg[:2])
+    def _parse_station_stop(self,msg,sendbackaddr):
+        self.rover_position = positions[msg[1]]
         msg_ack = 'STOP|ACK'
         msg_priority = Msgpriority.ACK
-        to_station = (msg_priority,msg_destination_station,msg_ack)
+        to_station = (msg_priority,sendbackaddr,msg_ack)
         to_rover = (Msgpriority.STOP,self.ROVER,'STOP')
         return[to_rover,to_station]
+
+    def _parse_station_rovercross(self,msg,sendbackaddr):
+        self.rover_position = positions[msg[1]]
+        if self.rover_destination == self.rover_position :
+            to_rover = (Msgpriority.SLOWDOWN,self.ROVER,'SLOWDOWN')
+            to_station = (Msgpriority.SLOWDOWN,sendbackaddr,"CROSS|YES")
+            return [to_rover,to_station]
+        else:
+            to_station = (Msgpriority.ACK,sendbackaddr,"CROSS|NO")
+            return [to_station]
 
 
 
     
     def parse_rover(self,msg:list):
         #TODO ACKS
+        if 'ACK' in msg:
+            print(" ".join(msg))
         #TODO Updates
-        pass
+        if 'STATE' in msg[-1]:
+            [_,state] = msg[-1].split(':')
+            state = int(state)
+            self.rover_state = RoverStates(state).value
+            print("rover State:" + RoverStates(state).name)
+
+
     def parse_junction(self,msg:list):
         #TODO UPDATES
+        state = int(msg[-1])
+        self.junction_state =  JunctionStates(state).value
+        print("JUNCTION: "+ JunctionStates(state).name)
+
+        if state == JunctionStates.ROVER_HERE.value:
+            if self.junction_position == 'STRAIGHT':
+                to_junction= (Msgpriority.GO,self.JUNCTION,"ROTATE|CROSS")
+            else :
+                to_junction= (Msgpriority.GO,self.JUNCTION,"ROTATE|STRAIGHT")
+            return [to_junction]
+
+        
         #TODO SLOWDOWN
+        if 'SLOWDOWN' in msg:
+            to_rover = (Msgpriority.SLOWDOWN, self.ROVER,"SLOWDOWN")
+            return [to_rover]
         #TODO STOP
+        if 'STOP' in msg:
+            to_rover = (Msgpriority.STOP,self.ROVER,"STOP")
+            return [to_rover]
         #TODO ROTATION|DONE
-        pass
+        if 'DONE' in msg:
+            if self.junction_position == 'STRAIGHT':
+                self.junction_position = "CROSS"
+            else:
+                self.junction_position = 'STRAIGHT'
+
+            if int(self.rover_destination) == 5:
+                if self.rover_destination == 5.1:
+                    to_rover = (Msgpriority.GO,self.ROVER,"FORWARD")
+                elif self.rover_destination == 5.2:
+                    to_rover = (Msgpriority.GO,self.ROVER,"REVERSE")
+            else:
+                direction = get_direction(5,self.rover_destination)
+                to_rover = (Msgpriority.GO,self.ROVER,direction)
+
+            return [to_rover]    
+                
+
     
     def parse_hmi(self,msg:list):
         #TODO CALL
+        if 'CALL' in msg:
+            called_station = msg[0]
+            self.callq.put(positions[called_station])
+            msg_call_ack = 'CALL|ACK'
+            msg_priority = Msgpriority.ACK
+            return [(msg_priority,self.HMI,msg_call_ack)]
         #TODO GO
         #TODO E_STOP
         #TODO RESTART
         #TODO UPDATES
 
-        pass
 
 
 
